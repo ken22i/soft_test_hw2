69e090ac3f5184edd4f219b0f509eb0afd40c925
ken22i <ken22i22i22i@gmail.com>
2023-10-31 13:39:10 +0800

    version1

---
+++ src/test/java/DayOfWeekTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
+public class DayOfWeekTest {
+
+  @ParameterizedTest
+  @CsvFileSource(resources = "/src/test/dates.csv")
+  void testDayOfWeek(int month, int day, String expectedDayOfWeek) {
+    LocalDate date = LocalDate.of(2021, month, day);
+    DayOfWeek actualDayOfWeek = date.getDayOfWeek();
+    assertEquals(expectedDayOfWeek, actualDayOfWeek.toString());
+  }
+}
